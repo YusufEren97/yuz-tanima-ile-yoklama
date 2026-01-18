d571b0cef6727cd87f971c30a03451133ad5fff6
YusufEren97 <yusuferenseyrek@gmail.com>
2026-01-18 04:41:21 +0300

    Fix: circular dependency hatası düzeltildi

--- backend/src/main/java/com/adiyaman/yoklama/config/SecurityConfig.java
+++ backend/src/main/java/com/adiyaman/yoklama/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.adiyaman.yoklama.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -19,6 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
+    @Lazy
     private final CustomLogoutSuccessHandler logoutSuccessHandler;
 
     @Bean
